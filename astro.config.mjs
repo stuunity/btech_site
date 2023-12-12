e3d40fb6ebeea86990febaa63edeb317ff02157d
Sukhpreet Singh <peadevp@gmail.com>
2023-12-12 18:22:29 +0530

    updated header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-    applyBaseStyles: false,
-  })]
+    applyBaseStyles: false
+  }), vue()]
 });
